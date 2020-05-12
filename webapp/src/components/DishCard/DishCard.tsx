7b103307f00dbf9a870483f53cc4f838772af009
Brandon Ling <brandonvling@gmail.com>
2020-05-11 23:20:48 -0400

    Use Dish interface

--- webapp/src/components/DishCard/DishCard.tsx
+++ webapp/src/components/DishCard/DishCard.tsx
@@ -1,30 +1,85 @@
 import React, { useState } from "react";
 import { useDrag } from "../../utility/useDrag";
 import "./DishCard.css";
-
-const images: string[] = [
-  "brooke-lark-V4MBq8kue3U-unsplash.jpg",
-  "casey-lee-awj7sRviVXo-unsplash.jpg",
-  "cayla1-w6ftFbPCs9I-unsplash.jpg",
-  "edgar-castrejon-1SPu0KT-Ejg-unsplash.jpg",
-  "joseph-gonzalez-zcUgjyqEwe8-unsplash.jpg",
-];
+import { Dish } from "../../utility/Dish";
 
 interface DishCard {
   url: string;
   name: string;
 }
 
+const dishes: Dish[] = [
+  {
+    name: "dish 0",
+    category: "food",
+    photo: {
+      id: "photo-0",
+      photographer: "photographer 0",
+      photographerProfileURL: "url-0",
+      url: "brooke-lark-V4MBq8kue3U-unsplash.jpg",
+    },
+  },
+  {
+    name: "dish 1",
+    category: "food",
+    photo: {
+      id: "photo-1",
+      photographer: "photographer 1",
+      photographerProfileURL: "url-1",
+      url: "casey-lee-awj7sRviVXo-unsplash.jpg",
+    },
+  },
+  {
+    name: "dish 2",
+    category: "food",
+    photo: {
+      id: "photo-2",
+      photographer: "photographer 2",
+      photographerProfileURL: "url-2",
+      url: "cayla1-w6ftFbPCs9I-unsplash.jpg",
+    },
+  },
+  {
+    name: "dish 3",
+    category: "food",
+    photo: {
+      id: "photo-3",
+      photographer: "photographer 3",
+      photographerProfileURL: "url-3",
+      url: "edgar-castrejon-1SPu0KT-Ejg-unsplash.jpg",
+    },
+  },
+  {
+    name: "dish 4",
+    category: "food",
+    photo: {
+      id: "photo-4",
+      photographer: "photographer 4",
+      photographerProfileURL: "url-4",
+      url: "joseph-gonzalez-zcUgjyqEwe8-unsplash.jpg",
+    },
+  },
+];
+
+// const images: string[] = [
+//   "brooke-lark-V4MBq8kue3U-unsplash.jpg",
+//   "casey-lee-awj7sRviVXo-unsplash.jpg",
+//   "cayla1-w6ftFbPCs9I-unsplash.jpg",
+//   "edgar-castrejon-1SPu0KT-Ejg-unsplash.jpg",
+//   "joseph-gonzalez-zcUgjyqEwe8-unsplash.jpg",
+// ];
+
 const DishCard = () => {
-  const [image, setImage] = useState(0);
+  const [dishNumber, setDishNumber] = useState(0);
+  // dishNumber must come before useDrag call
   const [deltaX, deltaY, dragDispatch] = useDrag();
 
   const changeImageHandler = (direction: string): void => {
-    setImage((image: number): number => {
+    setDishNumber((dishNumber: number): number => {
       if (direction === "forward") {
-        return (image + 1) % images.length;
+        return (dishNumber + 1) % dishes.length;
       } else {
-        return (image - 1 + images.length) % images.length;
+        return (dishNumber - 1 + dishes.length) % dishes.length;
       }
     });
   };
@@ -48,7 +103,8 @@ const DishCard = () => {
             }
           });
         }}
-        src={require(`../../assets/${images[image]}`)}
+        alt={dishes[dishNumber].name}
+        src={require(`../../assets/${dishes[dishNumber].photo.url}`)}
         style={{
           width: 0.85 * window.innerWidth,
           height: (3 * 0.85 * window.innerWidth) / 2,
@@ -56,7 +112,21 @@ const DishCard = () => {
           transform: `translate(${deltaX}px, ${deltaY}px)`,
         }}
       />
-      <figcaption>Dish name</figcaption>
+      <figcaption>{dishes[dishNumber].name}</figcaption>
+      <div>
+        Photo by{" "}
+        <a
+          href={
+            "https://unsplash.com/@anniespratt?utm_source=That Looks Good&utm_medium=referral"
+          }
+        >
+          {dishes[dishNumber].photo.photographer}
+        </a>{" "}
+        on{" "}
+        <a href="https://unsplash.com/?utm_source=That Looks Good&utm_medium=referral">
+          Unsplash
+        </a>
+      </div>
     </figure>
   );
 };
