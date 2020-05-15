9ad0588ee33d01ca45530c64a0f333fa10675e16
Brandon Ling <brandonvling@gmail.com>
2020-05-15 10:16:01 -0400

    Add test for unauthorized request

--- server/src/__tests__/graphql-server.test.ts
+++ server/src/__tests__/graphql-server.test.ts
@@ -2,6 +2,7 @@ import { createTestClient } from "apollo-server-testing";
 import { ApolloServer, gql } from "apollo-server-lambda";
 import { getDish } from "../query-resolvers";
 import { typeDefsString } from "../typeDefs";
+require("dotenv").config();
 
 const typeDefs = gql`
   ${typeDefsString}
@@ -13,7 +14,7 @@ const resolvers = {
   },
 };
 
-const testServer = new ApolloServer({
+const unAuthorizedUserServer = new ApolloServer({
   typeDefs,
   resolvers,
   context: () => ({
@@ -25,12 +26,25 @@ const testServer = new ApolloServer({
   }),
 });
 
+const authorizedUserServer = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: () => ({
+    event: {
+      headers: {
+        Authorization: `Bearer ${process.env.VALID_TEST_TOKEN}`,
+      },
+    },
+  }),
+});
+
 // jest.mock("../query-resolvers");
 // import { getDish } from "../query-resolvers";
 // import { server } from "../graphql-server";
 // import { gql } from "apollo-server-lambda";
 
-const { query } = createTestClient(testServer);
+const { query: unAuthorizedQuery } = createTestClient(unAuthorizedUserServer);
+const { query: authorizedQuery } = createTestClient(authorizedUserServer);
 
 const testResponse = {
   data: {
@@ -65,17 +79,9 @@ const GET_DISH = gql`
   }
 `;
 
-// const testServer = Object.create(Object.getPrototypeOf(server));
-// const unauthorizedContext = server.context();
-// // Need to create test server this way so that we can pass in headers, otherwise
-// // createTestClient will pass in empty object into context creator function
-// const { query, mutate } = createTestClient(
-//   Object.assign(testServer, server, { context: () => unauthorizedContext })
-// );
-
 describe("server", () => {
   test("rejects unauthorized requests", async () => {
-    const res = await query({
+    const res = await unAuthorizedQuery({
       query: GET_DISH,
       variables: {
         dishId: "3DZTercmEF4",
@@ -83,13 +89,13 @@ describe("server", () => {
     });
     expect(res.errors).toBeDefined();
     expect(
-      res.errors.map((err) => err.message).includes(`You are not authorized`)
+      res.errors.map((err) => err.message).includes(`You are not authorized.`)
     ).toBe(true);
     expect(res.data.getDish).toBeNull();
   });
 
-  // test("retrieves a dish from Unsplash", async () => {
-  //   const res = await query({
+  // test("retrieves a dish from Unsplash for authorized user", async () => {
+  //   const res = await authorizedQuery({
   //     query: GET_DISH,
   //     variables: {
   //       dishId: "3DZTercmEF4",
