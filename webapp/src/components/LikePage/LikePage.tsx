d638ea2ba2aecd24c1310473c6baaffc63378d99
Brandon Ling <brandonvling@gmail.com>
2020-05-18 20:44:53 -0400

    Separate LikePage into no likes, likes cases

--- webapp/src/components/LikePage/LikePage.tsx
+++ webapp/src/components/LikePage/LikePage.tsx
@@ -1,23 +1,22 @@
 import React from "react";
 import { Dish } from "../../utility/Dish";
+import "./LikePage.css";
 
 export const LikePage = ({ likedDishes }: { likedDishes: Dish[] }) => {
-  return (
-    <div className="like-page">
-      {likedDishes.length !== 0 ? (
-        <>
-          <p>You liked these dishes:</p>
-          <ul className="liked-dishes-list">
-            {likedDishes.map((dish) => (
-              <li className="liked-dishes-item" key={dish.photo.id}>
-                {dish.name}
-              </li>
-            ))}
-          </ul>
-        </>
-      ) : (
-        <p>You haven't liked any dishes yet</p>
-      )}
-    </div>
+  return likedDishes.length !== 0 ? (
+    <article className="like-page">
+      <p>You liked these dishes:</p>
+      <ul className="liked-dishes-list">
+        {likedDishes.map((dish) => (
+          <li className="liked-dishes-item" key={dish.photo.id}>
+            {dish.name}
+          </li>
+        ))}
+      </ul>
+    </article>
+  ) : (
+    <article className="no-likes-page">
+      <p>You haven't liked any dishes yet&hellip;</p>
+    </article>
   );
 };
