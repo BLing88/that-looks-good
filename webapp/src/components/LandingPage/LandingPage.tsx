6549581b8ea4f83ca7ecf0cc5093c12f263fc81d
Brandon Ling <brandonvling@gmail.com>
2020-05-27 22:01:58 -0400

    Style LandingPage

--- webapp/src/components/LandingPage/LandingPage.tsx
+++ webapp/src/components/LandingPage/LandingPage.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { RedirectLoginOptions } from "@auth0/auth0-spa-js";
+import styles from "./LandingPage.module.css";
 
 export const LandingPage = ({
   loginWithRedirect,
@@ -7,15 +8,24 @@ export const LandingPage = ({
   loginWithRedirect: (p?: RedirectLoginOptions) => Promise<void>;
 }) => {
   return (
-    <main>
-      <button
-        className="login-btn"
-        onClick={() => {
-          loginWithRedirect();
-        }}
-      >
-        Login
-      </button>
+    <main className={styles.landingPage}>
+      <div className={styles.top}>
+        <h1 className={styles.title}>That Looks Good</h1>
+        <button
+          className={styles.loginBtn}
+          onClick={() => {
+            loginWithRedirect();
+          }}
+        >
+          Login
+        </button>
+      </div>
+
+      <p className={styles.description}>
+        Hungry but don't know what to eat? We'll help you choose: just swipe
+        right on the dishes that look good and left on the ones that don't. Then
+        simply choose from the ones you liked!{" "}
+      </p>
     </main>
   );
 };
