e7745bacde592b2dfff06afc0295d41c7448e892
Brandon Ling <brandonvling@gmail.com>
2020-05-27 23:21:32 -0400

    Add changing photo animation to LandingPage

--- webapp/src/components/LandingPage/LandingPage.tsx
+++ webapp/src/components/LandingPage/LandingPage.tsx
@@ -1,12 +1,90 @@
-import React from "react";
+import React, { useReducer, useEffect, CSSProperties } from "react";
 import { RedirectLoginOptions } from "@auth0/auth0-spa-js";
 import styles from "./LandingPage.module.css";
+
+const photoUrls = [
+  "joseph-gonzalez-zcUgjyqEwe8-unsplash.jpg",
+  "casey-lee-awj7sRviVXo-unsplash.jpg",
+  "cayla1-w6ftFbPCs9I-unsplash.jpg",
+  "edgar-castrejon-1SPu0KT-Ejg-unsplash.jpg",
+];
+
+const animationTimeStep = 1000 / 60; // animation rate in milliseconds
+const transitionDuration = 600; // in milliseconds
+const maxAngle = 30; // max rotation angle in degrees
+const timeStep = 1500; // time between new photos, in milliseconds
+
+interface AnimationState {
+  xPosition: number;
+  photoUrlIndex: number;
+}
+
+const initialState = {
+  xPosition: 0,
+  photoUrlIndex: 0,
+};
+const animationReducer = (
+  state: AnimationState,
+  action: any
+): AnimationState => {
+  switch (action.type) {
+    case "animation update right":
+      return { ...state, xPosition: state.xPosition + 10 };
+    case "animation update left":
+      return { ...state, xPosition: state.xPosition - 10 };
+    case "next state":
+      return {
+        xPosition: 0,
+        photoUrlIndex: (state.photoUrlIndex + 1) % photoUrls.length,
+      };
+    default:
+      return initialState;
+  }
+};
 
 export const LandingPage = ({
   loginWithRedirect,
 }: {
   loginWithRedirect: (p?: RedirectLoginOptions) => Promise<void>;
 }) => {
+  const [state, dispatch] = useReducer(animationReducer, initialState);
+  const loadedStyle: CSSProperties = {
+    width: Math.min(200, Math.floor(0.85 * window.innerWidth)),
+    height: Math.min(300, Math.floor((3 * 0.85 * window.innerWidth) / 2)),
+    position: "relative",
+    transform: `
+    translate(${state.xPosition}px, 0px)
+    rotate(${
+      maxAngle * Math.tanh(state.xPosition / (window.innerWidth / 2))
+    }deg)`,
+  };
+
+  useEffect(() => {
+    let index = 0;
+    const animationInterval = setInterval(() => {
+      if (index % 2 === 0) {
+        const interval = setInterval(() => {
+          dispatch({ type: "animation update right" });
+        }, animationTimeStep);
+        setTimeout(() => {
+          clearInterval(interval);
+          index = (index + 1) % photoUrls.length;
+          dispatch({ type: "next state" });
+        }, transitionDuration);
+      } else {
+        const interval = setInterval(() => {
+          dispatch({ type: "animation update left" });
+        }, animationTimeStep);
+        setTimeout(() => {
+          clearInterval(interval);
+          index = (index + 1) % photoUrls.length;
+          dispatch({ type: "next state" });
+        }, transitionDuration);
+      }
+    }, timeStep);
+    return () => clearInterval(animationInterval);
+  }, []);
+
   return (
     <main className={styles.landingPage}>
       <div className={styles.top}>
@@ -20,6 +98,28 @@ export const LandingPage = ({
           Login
         </button>
       </div>
+
+      <figure className={styles.photos}>
+        <img
+          src={require(`../../assets/${photoUrls[state.photoUrlIndex]}`)}
+          alt="food"
+          style={loadedStyle}
+        />
+        <figcaption className={styles.attribution}>
+          Photo by{" "}
+          <a href="#" target="_blank" rel="noreferrer noopener">
+            Photographer
+          </a>{" "}
+          on{" "}
+          <a
+            href="https://unsplash.com/?utm_source=That Looks Good&utm_medium=referral"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            Unsplash
+          </a>
+        </figcaption>
+      </figure>
 
       <p className={styles.description}>
         Hungry but don't know what to eat? We'll help you choose: just swipe
